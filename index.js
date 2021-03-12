427876f9d2f7930c959c73827689222fd14b35d9
Jonathan Carnos <jonathan.carnos@gmail.com>
2021-03-12 19:09:50 +0100

    error response

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ mongoose.connect(process.env.MONGODB_URI, {
 const app = express(feathers());
 app.use(cors());
 app.use(formidable());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 
 class MessageService {
   constructor() {
